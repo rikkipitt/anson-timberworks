6cfb8890be79d5ea421a91bc969c16de45e7491d
Rikki Pitt <rikkipitt@gmail.com>
2018-11-28 15:30:15 +0000

    Fix some responsive issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,11 +2,10 @@ Rails.application.configure do
   config.cache_classes = false
   config.eager_load = false
   config.consider_all_requests_local = true
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  if Rails.root.join("tmp", "caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.cache_store = :memory_store
-    config.public_file_server.headers = {
-    }
+    config.public_file_server.headers = {}
   else
     config.action_controller.perform_caching = false
     config.cache_store = :null_store
@@ -27,12 +26,12 @@ Rails.application.configure do
   config.assets.quiet = true
   config.action_view.raise_on_missing_translations = true
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.default_url_options = {host: "localhost:3000"}
   config.action_mailer.asset_host = "http://localhost:3000"
-PAPERCLIP_STORAGE_OPTIONS = {
-  storage: :s3,
-  s3_credentials: "#{Rails.root}/config/s3.yml",
-  s3_host_name: 's3-eu-west-1.amazonaws.com',
-  s3_region: 'eu-west-1'
-}
+  PAPERCLIP_STORAGE_OPTIONS = {
+    storage: :s3,
+    s3_credentials: "#{Rails.root}/config/s3.yml",
+    s3_host_name: "s3-eu-west-1.amazonaws.com",
+    s3_region: "eu-west-1",
+  }
 end
