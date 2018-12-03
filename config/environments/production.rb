b4950767ed540f436cce7e8fb8b1c87a868b8798
Rikki Pitt <rikkipitt@gmail.com>
2018-12-03 16:03:35 +0000

    Update production force_ssl env var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,15 +2,15 @@ require Rails.root.join("config/smtp")
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
   config.assets.js_compressor = :uglifier
   config.assets.compile = false
   config.action_controller.asset_host = ENV.fetch("ASSET_HOST", ENV.fetch("APPLICATION_HOST"))
   config.active_storage.service = :local
   config.log_level = :debug
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = SMTP_SETTINGS
@@ -18,12 +18,12 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
   config.active_record.dump_schema_after_migration = false
-  
+
   if ENV.fetch("HEROKU_APP_NAME", "").include?("staging-pr-")
     ENV["APPLICATION_HOST"] = ENV["HEROKU_APP_NAME"] + ".herokuapp.com"
   end
@@ -32,14 +32,14 @@ Rails.application.configure do
   config.public_file_server.headers = {
     "Cache-Control" => "public, max-age=31557600",
   }
-  config.action_mailer.default_url_options = { host: ENV.fetch("APPLICATION_HOST") }
+  config.action_mailer.default_url_options = {host: ENV.fetch("APPLICATION_HOST")}
   config.action_mailer.asset_host = ENV.fetch("ASSET_HOST", ENV.fetch("APPLICATION_HOST"))
-  config.force_ssl = true
-PAPERCLIP_STORAGE_OPTIONS = {
-  storage: :s3,
-  s3_credentials: "#{Rails.root}/config/s3.yml",
-  s3_host_name: 's3-eu-west-1.amazonaws.com',
-  s3_region: 'eu-west-1',
-  s3_protocol: 'https'
-}
+  config.force_ssl = ENV["FORCE_SSL"].present?
+  PAPERCLIP_STORAGE_OPTIONS = {
+    storage: :s3,
+    s3_credentials: "#{Rails.root}/config/s3.yml",
+    s3_host_name: "s3-eu-west-1.amazonaws.com",
+    s3_region: "eu-west-1",
+    s3_protocol: "https",
+  }
 end
